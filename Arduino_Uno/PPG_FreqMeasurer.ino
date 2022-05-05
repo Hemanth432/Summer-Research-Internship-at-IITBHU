88fd2c720f7b316f38af0977266f397d58dd6bb3
Narra Hemanth Reddy <93421069+Hemanth432@users.noreply.github.com>
2022-05-05 15:52:44 +0530

    Add files via upload

---
+++ Arduino_Uno/PPG_FreqMeasurer.ino
@@ -0,0 +1,57 @@
+#include "arduinoFFT.h"
+ 
+#define SAMPLES 128             //Must be a power of 2
+#define SAMPLING_FREQUENCY 1000 //Hz, must be less than 10000 due to ADC
+ 
+arduinoFFT FFT = arduinoFFT();
+ 
+unsigned int sampling_period_us;
+unsigned long microseconds;
+ 
+double vReal[SAMPLES];
+double vImag[SAMPLES];
+
+float BPM;
+ 
+void setup() {
+    Serial.begin(115200);
+ 
+    sampling_period_us = round(1000000*(1.0/SAMPLING_FREQUENCY));
+}
+ 
+void loop() {
+   
+    /*SAMPLING*/
+    for(int i=0; i<SAMPLES; i++)
+    {
+        microseconds = micros();    //Overflows after around 70 minutes!
+     
+        vReal[i] = analogRead(0);
+        vImag[i] = 0;
+     
+        while(micros() < (microseconds + sampling_period_us)){
+        }
+    }
+ 
+    /*FFT*/
+    FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+    FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
+    FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
+    double peak = FFT.MajorPeak(vReal, SAMPLES, SAMPLING_FREQUENCY);     //Find out what frequency is the most dominant.
+ 
+    /*PRINT RESULTS*/
+    BPM = peak*60.0*1000;     //Calculate the heart-rate in terms of beats-per-minute (BPM).
+    Serial.println(BPM);     //Print out the heart-rate in terms of beats-per-minute (BPM).
+ 
+    //for(int i=0; i<(SAMPLES/2); i++)
+    //{
+        /*View all these three lines in serial terminal to see which frequencies has which amplitudes*/
+         
+        //Serial.print((i * 1.0 * SAMPLING_FREQUENCY) / SAMPLES, 1);
+        //Serial.print(" ");
+        //Serial.println(vReal[i], 1);    //View only this line in serial plotter to visualize the bins
+    //}
+ 
+    delay(1000);  //Repeat the process every second OR:
+    //while(1);       //Run code once
+}
